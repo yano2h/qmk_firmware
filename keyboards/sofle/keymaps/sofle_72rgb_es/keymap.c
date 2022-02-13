5bd407f2900154a43b8c6410788b03113e2e8016
Alejandro Alvarez <yano2h@gmail.com>
2022-02-13 19:50:42 -0300

    Remove erroneous characters

--- keyboards/sofle/keymaps/sofle_72rgb_es/keymap.c
+++ keyboards/sofle/keymaps/sofle_72rgb_es/keymap.c
@@ -106,8 +106,7 @@ const key_override_t shftRCBR_key_override = ko_make_basic(MOD_MASK_SHIFT, ES_RC
 // (shift + ; ) => :
 const key_override_t shftSCLN_key_override = ko_make_basic(MOD_MASK_SHIFT, ES_SCLN, ES_COLN);
 
-()?=
-()
+
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
     &shft6_key_override,
